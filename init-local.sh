0ec210344300239675acccb79c1ff737d47d29f2
Nijot <tomofumi.nijo@gmail.com>
2024-09-10 20:51:39 +0900

    Update init-local.sh

--- init-local.sh
+++ init-local.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-LOCAL_IP=$(curl http://169.254.169.254/latest/meta-data/local-ipv4)
+TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+LOCAL_IP=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/local-ipv4)
 
 sed -e s/LOCAL_IP/${LOCAL_IP}/g local-env.json.org > local-env.json
